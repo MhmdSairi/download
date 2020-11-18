14126f8e2be2b17497b897e951948114e56ada65
MhmdSairi <40028734+MhmdSairi@users.noreply.github.com>
2020-11-19 00:49:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if(isset($_POST['downloadBtn'])){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Image Download in PHP | CodingNepal</title>
+    <title>Image Downloader | MagerTools</title>
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
@@ -32,7 +32,7 @@ if(isset($_POST['downloadBtn'])){
             <div class="img-preview"></div>
             <div class="content">
                 <div class="img-icon"><i class="far fa-image"></i></div>
-                <div class="text">Paste the image url below, <br/>to see a preview or download!</div>
+                <div class="text">Paste url gambar/foto, <br/>Yang ingin kalian download</div>
             </div>
         </div>
         <form action="index.php" method="POST" class="input-data">
